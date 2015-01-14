d5e99c12162bc95a5877fada8fe1783c15036860
Izzulmakin <makin.ugm@gmail.com>
2015-01-14 15:17:39 +0700

    Fixes Popup FrameWindowH (hidesurrounding) to always created. also Separate datamaster as well

--- bukubesar/BukuBesarK.py
+++ bukubesar/BukuBesarK.py
@@ -1,15 +1,18 @@
-
+import MySQLdb
 from PyQt4.QtCore import QObject, pyqtSignal
 from PyQt4 import QtCore
 from PyQt4 import QtGui
 from PyQt4 import QtSvg
 from PyQt4.QtCore import * #nganggo QDateTime, ra apal e package ngarepe opo QtCore.QDateTime rung nyobo
 from PyQt4.QtGui import *
+import sip #lali nggo ngopo
+import sys,os
+import functools #partial
+import itertools #ubah tuple ke array
+import re #regular expression
 
 class BukuBesar(object):
-	def __init__():
-		return
-
+	
 	def BukuBesar_Goto(self,st_index):
 		self.st_BukuBesar.setCurrentIndex(st_index)
 		return
---
+++ datamaster/__init__.py
@@ -0,0 +1,1 @@
+from .DataMasterK import DataMaster
