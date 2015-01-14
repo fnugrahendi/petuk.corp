ae59732f07071734357b0034b6a5b9f60ed00d40
Izzulmakin <makin.ugm@gmail.com>
2015-01-14 14:32:12 +0700

    Pisah buku besar ke file sendiri buat modul buku besar

--- Garvin.py
+++ Garvin.py
@@ -12,6 +12,8 @@ import functools #partial
 import itertools #ubah tuple ke array
 import re #regular expression
 from GUI import Ui_MainWindow
+#----Buku Besar
+from bukubesar import BukuBesar
 
 try:
 	_fromUtf8 = QtCore.QString.fromUtf8
@@ -20,7 +22,7 @@ except AttributeError:
 		return s
 
 
-class MainGUI(QtGui.QMainWindow, Ui_MainWindow):
+class MainGUI(QtGui.QMainWindow, Ui_MainWindow,BukuBesar):
 	def __init__(self, parent= None):
 		super(MainGUI,self).__init__(parent)
 		self.setupUi(self)
@@ -2232,136 +2234,6 @@ class MainGUI(QtGui.QMainWindow, Ui_MainWindow):
 	def Penjualan_GoTo_PembayaranPiutang_Baru(self):
 		self.st_Penjualan.setCurrentIndex(self.INDEX_ST_PENJUALAN_PPB)
 
-
-
-	#------------------------------------------------------------------- Buku Besar
-	#------------------------------------------------------------------- Buku Besar
-	def BukuBesar_Goto(self,st_index):
-		self.st_BukuBesar.setCurrentIndex(st_index)
-		return
-	
-	def BukuBesar_DaftarTransaksiJurnal(self):
-		"""Draw info Daftar Transaksi Jurnal """
-		self.st_BukuBesar.setCurrentIndex(self.INDEX_ST_BUKUBESAR_DAFTARTRANSAKSIJURNAL)
-		result = self.DatabaseRunQuery("SELECT * FROM `gd_transaksi_jurnal`")
-		field = self.BukuBesar_TransaksiJurnal_Field.index
-		CTANGGAL = 0
-		CNOMOR_REFERENSI = 1
-		CKETERANGAN = 2
-		CNILAI = 3
-		self.tbl_BukuBesar_DaftarTransaksiJurnal_Fcontent_List.setRowCount(len(result))
-		for row in range(0,len(result)):
-			
-			if (self.tbl_BukuBesar_DaftarTransaksiJurnal_Fcontent_List.item(row,CTANGGAL)==None):
-				item = QtGui.QTableWidgetItem()
-				self.tbl_BukuBesar_DaftarTransaksiJurnal_Fcontent_List.setItem(row, CTANGGAL, item)
-			if (self.tbl_BukuBesar_DaftarTransaksiJurnal_Fcontent_List.item(row,CNOMOR_REFERENSI)==None):
-				item = QtGui.QTableWidgetItem()
-				self.tbl_BukuBesar_DaftarTransaksiJurnal_Fcontent_List.setItem(row, CNOMOR_REFERENSI, item)
-			if (self.tbl_BukuBesar_DaftarTransaksiJurnal_Fcontent_List.item(row,CKETERANGAN)==None):
-				item = QtGui.QTableWidgetItem()
-				self.tbl_BukuBesar_DaftarTransaksiJurnal_Fcontent_List.setItem(row, CKETERANGAN, item)
-			if (self.tbl_BukuBesar_DaftarTransaksiJurnal_Fcontent_List.item(row,CNILAI)==None):
-				item = QtGui.QTableWidgetItem()
-				self.tbl_BukuBesar_DaftarTransaksiJurnal_Fcontent_List.setItem(row, CNILAI, item)
-			
-			item = self.tbl_BukuBesar_DaftarTransaksiJurnal_Fcontent_List.item(row,CTANGGAL)
-			item.setText(str(result[row][field("tanggal")]))
-			item = self.tbl_BukuBesar_DaftarTransaksiJurnal_Fcontent_List.item(row,CNOMOR_REFERENSI)
-			item.setText(str(result[row][CNOMOR_REFERENSI]))
-			item = self.tbl_BukuBesar_DaftarTransaksiJurnal_Fcontent_List.item(row,CKETERANGAN)
-			item.setText(str(result[row][CKETERANGAN]))
-			item = self.tbl_BukuBesar_DaftarTransaksiJurnal_Fcontent_List.item(row,CNILAI)
-			item.setText(str(result[row][CNILAI]))
-		data = None
-		def _SetActiveIndex(a,b):
-			return
-			nomorreferensi = str(self.tbl_BukuBesar_DaftarTransaksiJurnal_Fcontent_List.item(a,CNOMOR_REFERENSI).text())
-			data = self.DatabaseRunQuery("SELECT * FROM `gd_transaksi_jurnal` WHERE `kodeTransaksi` LIKE '"+nomorreferensi+"' ;")[0] #id always field 0
-			self.BukuBesar_DaftarTransaksiJurnal_idEDIT = data[0]
-			
-		def _EditCertainCell(a,b):
-			nomorreferensi = str(self.tbl_BukuBesar_DaftarTransaksiJurnal_Fcontent_List.item(a,CNOMOR_REFERENSI).text())
-			data = self.DatabaseRunQuery("SELECT * FROM `gd_transaksi_jurnal` WHERE `kodeTransaksi` LIKE '"+nomorreferensi+"' ;")[0]
-			self.BukuBesar_DaftarTransaksiJurnal_idEDIT = data[0]
-			self.BukuBesar_DaftarTransaksiJurnal_Tambah(data)
-			return
-		
-		self.tbl_BukuBesar_DaftarTransaksiJurnal_Fcontent_List.setColumnWidth(0,400)#check this miracle out!
-		
-		#--------------------Data Rekening tabel
-		try:
-			self.tbl_BukuBesar_DaftarTransaksiJurnal_Fcontent_List.cellClicked.disconnect()
-		except:
-			pass
-		try:
-			self.tbl_BukuBesar_DaftarTransaksiJurnal_Fcontent_List.cellDoubleClicked.disconnect()
-		except:
-			pass
-		self.tbl_BukuBesar_DaftarTransaksiJurnal_Fcontent_List.cellClicked.connect(_SetActiveIndex)
-		self.tbl_BukuBesar_DaftarTransaksiJurnal_Fcontent_List.cellDoubleClicked.connect(_EditCertainCell)
-		
-	def BukuBesar_DaftarTransaksiJurnal_Tambah(self,dataTransaksiJurnal=None):
-		""" masuk & kontrol Room tambah Daftar Transaksi jurnal """
-		self.st_BukuBesar.setCurrentIndex(self.INDEX_ST_BUKUBESAR_DAFTARTRANSAKSIJURNAL_TAMBAH)
-		field = self.BukuBesar_DetailTransaksiJurnal_Field.index
-		CKODE_AKUN = 0
-		CNAMA_AKUN = 1
-		CDEPARTEMEN = 2
-		CDEBIT = 3
-		CKREDIT = 4
-		
-		self.le_BukuBesar_DaftarTransaksiJurnal_Tambah_NomorReferensi.setText(dataTransaksiJurnal[self.BukuBesar_TransaksiJurnal_Field.index("kodeTransaksi")])
-		self.dte_BukuBesar_DaftarTransaksiJurnal_Tambah_Tanggal.setDateTime(QDateTime.fromString(str(dataTransaksiJurnal[self.BukuBesar_TransaksiJurnal_Field.index("tanggal")]),"yyyy-MM-dd hh:mm:ss"))
-		self.le_BukuBesar_DaftarTransaksiJurnal_Tambah_Keterangan.setText(dataTransaksiJurnal[self.BukuBesar_TransaksiJurnal_Field.index("catatan")])
-		
-		if (self.BukuBesar_DaftarTransaksiJurnal_idEDIT > -1):
-			sql = "SELECT * FROM `gd_detail_transaksi_jurnal` WHERE `kodeTransaksi` LIKE '"+str(self.le_BukuBesar_DaftarTransaksiJurnal_Tambah_NomorReferensi.text())+"' ;"
-			result = self.DatabaseRunQuery(sql)
-			self.tbl_BukuBesar_DaftarTransaksiJurnal_Tambah_List.setRowCount(len(result))
-			for r in range(0,len(result)):
-				if (self.tbl_BukuBesar_DaftarTransaksiJurnal_Tambah_List.item(r,CKODE_AKUN)==None):
-					item = QtGui.QTableWidgetItem()
-					self.tbl_BukuBesar_DaftarTransaksiJurnal_Tambah_List.setItem(r, CKODE_AKUN, item)
-				if (self.tbl_BukuBesar_DaftarTransaksiJurnal_Tambah_List.item(r,CNAMA_AKUN)==None):
-					item = QtGui.QTableWidgetItem()
-					self.tbl_BukuBesar_DaftarTransaksiJurnal_Tambah_List.setItem(r, CNAMA_AKUN, item)
-				if (self.tbl_BukuBesar_DaftarTransaksiJurnal_Tambah_List.item(r,CDEPARTEMEN)==None):
-					item = QtGui.QTableWidgetItem()
-					self.tbl_BukuBesar_DaftarTransaksiJurnal_Tambah_List.setItem(r, CDEPARTEMEN, item)
-				if (self.tbl_BukuBesar_DaftarTransaksiJurnal_Tambah_List.item(r,CDEBIT)==None):
-					item = QtGui.QTableWidgetItem()
-					self.tbl_BukuBesar_DaftarTransaksiJurnal_Tambah_List.setItem(r, CDEBIT, item)
-				if (self.tbl_BukuBesar_DaftarTransaksiJurnal_Tambah_List.item(r,CKREDIT)==None):
-					item = QtGui.QTableWidgetItem()
-					self.tbl_BukuBesar_DaftarTransaksiJurnal_Tambah_List.setItem(r, CKREDIT, item)
-					
-				self.tbl_BukuBesar_DaftarTransaksiJurnal_Tambah_List.item(r,CKODE_AKUN).setText(str(result[r][field("noAkunJurnal")]))
-				sql = "SELECT * FROM `gd_rekening_jurnal` WHERE `noAkun` LIKE '"+str(result[r][field("noAkunJurnal")])+"' ;"
-				namaAkun = self.DatabaseRunQuery(sql)[0][self.DataMaster_DataRekening_Field.index("namaAkun")]
-				self.tbl_BukuBesar_DaftarTransaksiJurnal_Tambah_List.item(r,CNAMA_AKUN).setText(str(namaAkun))
-				self.tbl_BukuBesar_DaftarTransaksiJurnal_Tambah_List.item(r,CDEPARTEMEN).setText(str(result[r][field("kodeDepartemen")]))
-				self.tbl_BukuBesar_DaftarTransaksiJurnal_Tambah_List.item(r,CDEBIT).setText(str(result[r][field("debit")]))
-				self.tbl_BukuBesar_DaftarTransaksiJurnal_Tambah_List.item(r,4).setText(str(result[r][field("kredit")]))
-		
-		try:
-			self.tbl_BukuBesar_DaftarTransaksiJurnal_Tambah_List.cellClicked.disconnect()
-		except:
-			pass
-		try:
-			self.tbl_BukuBesar_DaftarTransaksiJurnal_Tambah_List.cellDoubleClicked.disconnect()
-		except:
-			pass
-		self.tbl_BukuBesar_DaftarTransaksiJurnal_Tambah_List.cellDoubleClicked.connect(self.BukuBesar_DaftarTransaksiJurnal_PilihRekening)
-		
-		
-		return
-	
-	def BukuBesar_DaftarTransaksiJurnal_PilihRekening(self,row,column):
-		
-		self.DataMaster_DataRekening_Popup_Pilih()
-		
-	
 	def DatabaseRunQuery(self,query):
 		self.initDatabase()
 		cursor = self.db.cursor()
---
+++ bukubesar/__init__.py
@@ -0,0 +1,1 @@
+from .kontrol import BukuBesar
