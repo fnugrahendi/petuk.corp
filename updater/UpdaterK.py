b3185d7995a1e0f4b8fddd5b79031551bb78b448
Izzulmakin <makin.ugm@gmail.com>
2015-01-26 23:09:15 +0700

    tes check function from online file source

--- updater/UpdaterK.py
+++ updater/UpdaterK.py
@@ -1,6 +1,23 @@
 import wget
+import re
 
 class Updater(object):
 	def __init__(self, parent=None):
 		pass
-	
+	def GarvinCheckVersion(self):
+		sukses = True
+		try:
+			import urllib
+		except:
+			print "install urllib yoh, kiye dinggo updater"
+			return False
+
+		f = urllib.urlopen("https://github.com/fnugrahendi/petuk.corp/blob/master/currentversion.md")
+		data = f.read()
+		f.close()
+		versi = re.findall("current garvin version is (\d+\.\d+) today is",data)
+		if (len(versi)<1):
+			return False
+		else:
+			versi = versi[0]
+		
