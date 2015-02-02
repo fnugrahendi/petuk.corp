e2edc2db6caa391883523a6efaa81eb1b4fd5638
Izzulmakin <makin.ugm@gmail.com>
2015-02-02 20:30:02 +0700

    removed dlls

--- garvinlibrary/src/ConfigLoader.cpp
+++
@@ -1,9 +0,0 @@
-#ifdef NGANGGOLINUX
-	#define DLLEXPORT extern "C" 
-#else
-	#define DLLEXPORT extern "C" __declspec(dllexport) 		
-#endif
-
-DLLEXPORT int tambah(int a, int b) {
-    return a + b;
-}
