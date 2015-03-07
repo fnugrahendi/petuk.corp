453dd164b5f2c80098c4e96d9dab32c326651ef4
Izzulmakin <makin.ugm@gmail.com>
2015-03-07 16:20:04 +0700

    cek download sudah

--- test/updateinstaller/src/updateinstaller.cpp
+++ test/updateinstaller/src/updateinstaller.cpp
@@ -1,13 +1,17 @@
 #include <string>
 #include <iostream>
 #include <stdio.h>
+#include <csignal>
+
+#include <fstream>
+
 #define WINDOWS
 #ifdef WINDOWS
 #define popen _popen
 #define pclose _pclose
 
 #endif
-
+#define umpomo if
 
 std::string exec(char* cmd) {
     FILE* pipe = popen(cmd, "r");
@@ -22,8 +26,61 @@ std::string exec(char* cmd) {
     return result;
 }
 
-int main(int argc, char* argkvlt[])
+int main(int argc, char* kvlt[])
 {
-	std::cout<<argkvlt[0]<<"\n";
+	//~ std::cout<<exec(argkvlt[1])<<"\n";
+	//~ std::cin>>argc;
+	//~ std::cout<<argc<<"\n";
+	//~ for (int x=0;x<argc;x++)
+	//~ {
+		//~ std::cout<<x<<" "<<kvlt[x]<<"\n";
+	//~ }
+	//~ std::cin>>argc;
+	//~ std::string isi = kvlt[1];
+	//~ std::string cari = kvlt[2];
+	//~ std::size_t found = isi.find(cari);
+	
+	bool downloaddone=true;
+	for (int x=1;x<argc;x++)
+	{		
+		try
+		{
+			std::string line;
+			std::ifstream myfile (kvlt[x]);
+			if (myfile.is_open())
+			{
+				bool thisfiledone = false;
+				while ( std::getline (myfile,line) )
+				{
+					if (line.find("saved")!=std::string::npos)
+					{
+						///-- ketemu
+						std::cout<<"file "<<kvlt[x]<<" sudah selesai didownload \n";
+						thisfiledone = true;
+					}
+				}
+				myfile.close();
+				if (!thisfiledone) ///nek thisfiledone jangan buat downloaddone=false
+				{
+					std::cout<<"file "<<kvlt[x]<<" belum selesai didownload \n";
+					downloaddone = false;
+				}
+			}
+			else
+			{
+				std::cout<<"file "<<kvlt[x]<<" belum mulai didownload \n";
+				downloaddone = false;
+			}
+		}
+		catch (...)
+		{
+			;;
+		}
+	}
+	umpomo (downloaddone)
+	{
+		std::cout<<"rampung kabeh, extraksi hendakny\n";
+	}
+	std::cin>>argc;
 	return (0);
 }
