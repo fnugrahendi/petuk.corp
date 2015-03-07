1c48dc70999877e2f26ee6eabfd6f36c46d3de65
Izzulmakin <makin.ugm@gmail.com>
2015-03-07 18:16:56 +0700

    path currently working, tinggal extraksi

--- test/updateinstaller/src/updateinstaller.cpp
+++ test/updateinstaller/src/updateinstaller.cpp
@@ -2,16 +2,23 @@
 #include <iostream>
 #include <stdio.h>
 #include <csignal>
-
 #include <fstream>
 
 #define WINDOWS
 #ifdef WINDOWS
-#define popen _popen
-#define pclose _pclose
-
+	#define popen _popen
+	#define pclose _pclose
+	#include <windows.h>
+	#define delay(x) Sleep(x)
+	#define CEKPROSES "wmic process get description,executablepath"
+#else
+	#include <unistd.h> //posix
+	#define delay(x) usleep(x*1000)
+	#define CEKPROSES "ps -ef | grep Garvin"
 #endif
+	
 #define umpomo if
+#define njerone(panggongolek,singdigoleti) panggongolek.find(singdigoleti)!=std::string::npos
 
 std::string exec(char* cmd) {
     FILE* pipe = popen(cmd, "r");
@@ -26,8 +33,11 @@ std::string exec(char* cmd) {
     return result;
 }
 
+
 int main(int argc, char* kvlt[])
 {
+	std::string BasePath;
+	BasePath = kvlt[0];
 	//~ std::cout<<exec(argkvlt[1])<<"\n";
 	//~ std::cin>>argc;
 	//~ std::cout<<argc<<"\n";
@@ -40,47 +50,57 @@ int main(int argc, char* kvlt[])
 	//~ std::string cari = kvlt[2];
 	//~ std::size_t found = isi.find(cari);
 	
+	
 	bool downloaddone=true;
-	for (int x=1;x<argc;x++)
-	{		
-		try
-		{
-			std::string line;
-			std::ifstream myfile (kvlt[x]);
-			if (myfile.is_open())
-			{
-				bool thisfiledone = false;
-				while ( std::getline (myfile,line) )
-				{
-					if (line.find("saved")!=std::string::npos)
-					{
-						///-- ketemu
-						std::cout<<"file "<<kvlt[x]<<" sudah selesai didownload \n";
-						thisfiledone = true;
-					}
-				}
-				myfile.close();
-				if (!thisfiledone) ///nek thisfiledone jangan buat downloaddone=false
-				{
-					std::cout<<"file "<<kvlt[x]<<" belum selesai didownload \n";
-					downloaddone = false;
-				}
-			}
-			else
-			{
-				std::cout<<"file "<<kvlt[x]<<" belum mulai didownload \n";
-				downloaddone = false;
-			}
-		}
-		catch (...)
-		{
-			;;
-		}
-	}
-	umpomo (downloaddone)
+	do
 	{
-		std::cout<<"rampung kabeh, extraksi hendakny\n";
+		downloaddone=true;
+		for (int x=1;x<argc;x++)
+		{
+			try
+			{
+				std::string line;
+				std::ifstream myfile (kvlt[x]);
+				if (myfile.is_open())
+				{
+					bool thisfiledone = false;
+					while ( std::getline (myfile,line) )
+					{
+						if (line.find("saved")!=std::string::npos)
+						{
+							///-- ketemu
+							std::cout<<"file "<<kvlt[x]<<" sudah selesai didownload \n";
+							thisfiledone = true;
+						}
+					}
+					myfile.close();
+					if (!thisfiledone) ///nek thisfiledone jangan buat downloaddone=false
+					{
+						std::cout<<"file "<<kvlt[x]<<" belum selesai didownload \n";
+						downloaddone = false;
+					}
+				}/**if (myfile.is_open())**/
+				else
+				{
+					std::cout<<"file "<<kvlt[x]<<" belum mulai didownload \n";
+					downloaddone = false;
+				}/**if (myfile.is_open())**/
+			}/**try**/
+			catch (...)
+			{
+				;;
+			}/**try**/
+		}/**for (int x=1;x<argc;x++) // perulangan tiap argumen/file yang dicek**/
+		delay(3000);
 	}
+	while (!downloaddone);
+	
+	while (njerone(exec(CEKPROSES),"Garvin"))
+	{
+		std::cout<<"download selesai, pemasangan update akan dilakukan bila pengguna sudah siap (sudah menutup Garvin)\n";		
+		delay(3000);
+	}
+	std::cout<<"Memasang update\n";
 	std::cin>>argc;
 	return (0);
 }
