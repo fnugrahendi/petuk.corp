bf7902383abd23b58017f51b9b453c5185dd7ed7
Izzulmakin <makin.ugm@gmail.com>
2015-04-11 16:39:30 +0700

    first startup software setting up

--- source/Garvin.py
+++ source/Garvin.py
@@ -62,15 +62,16 @@ class MainGUI(QtGui.QMainWindow, Ui_MainWindow,BukuBesar,DataMaster,Penjualan,Pe
 		self.GarvinCheckIsUpdated()
 		
 		#--- perlu ada reset versi garvin, dipanggil dengan melewatkan argumen pada pemanggilan garvin dengan argumen seperti yang tertera dibawah
-		#~ if len(sys.argv)>1:
-			#~ if sys.argv[1]=="-RESETGARVIN":
-				#~ self.DataMaster_Popup("Anda akan mereset Garvin ke versi awal")
-				
-		#~ if (self.GarvinGetConfig("FILE VERSION","bin/garvinbin.dat")==""):
-			#~ print ("File version gak ada, menuliskan")
-			#~ dataversi = "versiini = [['garvin', 1, 'localhost'],['bin',  1, 'localhost'],['data',  1, 'localhost'],['doc',  1, 'localhost'],['image',  1, 'localhost'],['installer', 1, 'localhost'],['mysql',  1, 'localhost'],['source',  1, 'localhost']]" #-- lgsg executable array asignment
-			#~ self.GarvinSetConfig("FILE VERSION",dataversi,"bin/garvinbin.dat")
-			#~ print ("done.")
+		if len(sys.argv)>1:
+			if sys.argv[1]=="--reset-garvin":
+				print "Anda akan mereset Garvin ke versi awal"
+				self.DataMaster_Popup("Anda akan mereset Garvin ke versi awal")
+				#~ 
+		if (self.GarvinGetConfig("FILE VERSION","bin/garvinbin.dat")==""):
+			print ("File version gak ada, menuliskan")
+			dataversi = "versiini = [['garvin', 1, 'localhost'],['bin',  1, 'localhost'],['data',  1, 'localhost'],['doc',  1, 'localhost'],['image',  1, 'localhost'],['installer', 1, 'localhost'],['mysql',  1, 'localhost'],['source',  1, 'localhost']]" #-- lgsg executable array asignment
+			self.GarvinSetConfig("FILE VERSION",dataversi,"bin/garvinbin.dat")
+			print ("done.")
 	
 	def GarvinInit(self):
 		#-- init dipindah disini, karena dipanggil setelah berhasil login (set database dsb) di fungsi self.Login_Done
